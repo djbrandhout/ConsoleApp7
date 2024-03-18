8acbbbbe94ddaf3511c9f616635099373d2d91f0
evanhoek <163893795+evanhoek@users.noreply.github.com>
2024-03-18 17:55:27 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("poyyy");
 Console.WriteLine("Novo");
+Console.WriteLine("ovo dodaje kolaborator");
+#added collab line
